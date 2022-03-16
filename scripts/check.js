8572d9fd3419a76684611f7142824397bfe1664f
"Carbonbirdman" <"carbonbirdman@gmail.com">
2022-03-16 13:34:35 +1100

    add check

---
+++ scripts/check.js
@@ -0,0 +1,31 @@
+const hre = require("hardhat");
+const fs = require("fs");
+require("dotenv").config();
+
+let config,arb,owner;
+const network = hre.network.name;
+if (network === 'aurora') config = require('./../config/aurora.json');
+if (network === 'fantom') config = require('./../config/fantom.json');
+
+const main = async () => {
+  [owner] = await ethers.getSigners();
+  console.log(`Owner: ${owner.address}`);
+	
+}
+
+process.on('uncaughtException', function(err) {
+	console.log('UnCaught Exception 83: ' + err);
+	console.error(err.stack);
+	fs.appendFile('./critical.txt', err.stack, function(){ });
+});
+
+process.on('unhandledRejection', (reason, p) => {
+	console.log('Unhandled Rejection at: '+p+' - reason: '+reason);
+});
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
